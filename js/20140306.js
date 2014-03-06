7fcc0db68a05d03faabf0f3d059ac302b1302a50
brentajones <bjones@bjones.net>
2014-03-06 13:28:30 -0600

    Commit

---
+++ js/20140306.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for the week of Mar 3, 2014",
+        "main":    "<p>From Downtown to the airport, across the state and even into Canada, how well were you paying attention to this week's news? Take our quiz to find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+    { 
+        "q": "Officials at Lambert Airport announced changes in what service this week?",
+        "a": [
+            {"option": "Parking",      "correct": false},
+            {"option": "Wi-Fi",     "correct": false},
+            {"option": "Taxis",      "correct": false},
+            {"option": "Baggage handling",     "correct": true} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/lambert-airport-rolls-out-baggage-screening-system-plans-new-copper-roof&quot;>Reporter Joseph Leahy detailed plans for a $50.7 million assembly of ramps, conveyor belts and X-rays that will allow passengers to once again leave their luggage with a ticket agent instead of having to take bags to screening machines themselves.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/lambert-airport-rolls-out-baggage-screening-system-plans-new-copper-roof&quot;>Reporter Joseph Leahy detailed plans for a $50.7 million assembly of ramps, conveyor belts and X-rays that will allow passengers to once again leave their luggage with a ticket agent instead of having to take bags to screening machines themselves.</a></p>" // no comma here
+    },
+    { 
+        "q": "Besides Taste of St. Louis, what other festival will be moving to Chesterfield from the city this summer?",
+        "a": [
+            {"option": "Bluesweek",      "correct": true},
+            {"option": "Fair St. Louis",     "correct": false},
+            {"option": "Strassenfest",      "correct": false},
+            {"option": "Ribfest",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/taste-st-louis-second-festival-leave-downtown-week&quot;>Bluesweek also is moving to the Chesterfield amphitheater from downtown. The announcements came as a bill was introduced in the Board of Aldermen that could lead to big-name music festivals in the city on the Memorial Day and Labor Day weekends, according to reporter Rachel Lippmann.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/taste-st-louis-second-festival-leave-downtown-week&quot;>Bluesweek also is moving to the Chesterfield amphitheater from downtown. The announcements came as a bill was introduced in the Board of Aldermen that could lead to big-name music festivals in the city on the Memorial Day and Labor Day weekends, according to reporter Rachel Lippmann.</a></p>" // no comma here
+    },
+    { 
+        "q": "The St. Louis Symphony announced that music director David Robertson is now under contract through what year?",
+        "a": [
+            {"option": "2017",      "correct": false},
+            {"option": "2018",     "correct": true},
+            {"option": "2019",      "correct": false},
+            {"option": "2020",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/reflection-robertsons-contract-reinforces-infrastructure-st-louis&quot;>Robertson, who has led the orchestra since 2005, signed a new contract through the 2017-18 season. Read Robert Duffy’s appreciation of his leadership and artistry.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/reflection-robertsons-contract-reinforces-infrastructure-st-louis&quot;>Robertson, who has led the orchestra since 2005, signed a new contract through the 2017-18 season. Read Robert Duffy’s appreciation of his leadership and artistry.</a></p>" // no comma here
+    },
+    { 
+        "q": "Why did Missouri education officials reduce the number of tests students will have to take?",
+        "a": [
+            {"option": "Money",      "correct": false},
+            {"option": "Loss of teaching time",     "correct": false},
+            {"option": "Complaints from education groups",      "correct": false},
+            {"option": "All of the above",     "correct": true} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/missouri-reconsiders-how-many-tests-students-must-take&quot;>All of those factors – the costs of the tests, reduced teaching time and concerns from groups representing teachers, administrators and school boards – led to the new testing plan, as education reporter Dale Singer detailed.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/missouri-reconsiders-how-many-tests-students-must-take&quot;>All of those factors – the costs of the tests, reduced teaching time and concerns from groups representing teachers, administrators and school boards – led to the new testing plan, as education reporter Dale Singer detailed.</a></p>" // no comma here
+    },
+    { 
+        "q": "Legislation advanced in the Missouri House to proclaim a cease-fire in the “border war” with what neighboring state?",
+        "a": [
+            {"option": "Illinois",      "correct": false},
+            {"option": "Iowa",     "correct": false},
+            {"option": "Kansas",      "correct": true},
+            {"option": "Arkansas",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/missouri-house-passes-border-war-cease-fire-bill&quot;>The bill would ban Missouri from offering economic incentives to businesses in four Kansas counties in the Kansas City area, but as reporter Marshall Griffin explained, it would require Kansas to pass a similar law for businesses in Missouri.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/missouri-house-passes-border-war-cease-fire-bill&quot;>The bill would ban Missouri from offering economic incentives to businesses in four Kansas counties in the Kansas City area, but as reporter Marshall Griffin explained, it would require Kansas to pass a similar law for businesses in Missouri.</a></p>" // no comma here
+    },
+    { 
+        "q": "Ellisville mayor Adam Paul has filed to run for a seat in what body?",
+        "a": [
+            {"option": "St. Louis County Council",      "correct": true},
+            {"option": "Missouri House",     "correct": false},
+            {"option": "Missouri Senate",      "correct": false},
+            {"option": "None of the above &mdash; he’s leaving politics altogether",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/ellisville-mayor-files-st-louis-county-council-area-republicans-regroup&quot;>Paul is running for the seat being left vacant by St. Louis County Councilman Greg Quinn, who is retiring. Jo Mannies reported he is the third Republican who has filed for the position.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/ellisville-mayor-files-st-louis-county-council-area-republicans-regroup&quot;>Paul is running for the seat being left vacant by St. Louis County Councilman Greg Quinn, who is retiring. Jo Mannies reported he is the third Republican who has filed for the position.</a></p>" // no comma here
+    },
+    { 
+        "q": "Gov. Jay Nixon announced a trade agreement between Missouri and what Canadian province?",
+        "a": [
+            {"option": "Alberta",      "correct": false},
+            {"option": "Quebec",     "correct": true},
+            {"option": "British Columbia",      "correct": false},
+            {"option": "Saskatchewan",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/nixon-announces-trade-accord-between-missouri-and-quebec&quot;>The pact is between Missouri and Quebec. Speaking to reporters from his trade mission to Canada, Nixon said the agreement is designed to boost trade over the next four years by 15 percent. Jason Rosenbaum reported that Canada is Missouri’s biggest export market.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/nixon-announces-trade-accord-between-missouri-and-quebec&quot;>The pact is between Missouri and Quebec. Speaking to reporters from his trade mission to Canada, Nixon said the agreement is designed to boost trade over the next four years by 15 percent. Jason Rosenbaum reported that Canada is Missouri’s biggest export market.</a></p>" // no comma here
+    },
+    { 
+        "q": "The late Rev. James F. DeClue, former head of the St. Louis chapter of the NAACP, worked for 32 years at what local company?",
+        "a": [
+            {"option": "Anheuser-Busch",      "correct": false},
+            {"option": "Emerson Electric",     "correct": true},
+            {"option": "Laclede Gas",      "correct": false},
+            {"option": "Monsanto",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/rev-james-f-declue-headed-local-naacp-worked-employment&quot;>DeClue spent 32 years at Emerson, ending his career as corporate director of affirmative action and equal employment opportunity. He also led  Washington Tabernacle Missionary Baptist Church and New Bethel Missionary Baptist Church. He died recently at age 86. Read our obituary by Gloria Ross.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/rev-james-f-declue-headed-local-naacp-worked-employment&quot;>DeClue spent 32 years at Emerson, ending his career as corporate director of affirmative action and equal employment opportunity. He also led  Washington Tabernacle Missionary Baptist Church and New Bethel Missionary Baptist Church. He died recently at age 86. Read our obituary by Gloria Ross.</a></p>" // no comma here
+    },
+    { 
+        "q": "What caused the fire at the Bridgeton Landfill",
+        "a": [
+            {"option": "Lightning",      "correct": false},
+            {"option": "Improper dumping",     "correct": false},
+            {"option": "A chemical reaction",      "correct": true},
+            {"option": "Careless smoking",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/confused-about-bridgeton-and-west-lake-landfills-heres-what-you-should-know&quot;>The fire isn’t the traditional type, with smoke and flame. As science reporter Veronique LaCapra explained, it’s a chemical reaction that produces temperatures higher than 170 degrees.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/confused-about-bridgeton-and-west-lake-landfills-heres-what-you-should-know&quot;>The fire isn’t the traditional type, with smoke and flame. As science reporter Veronique LaCapra explained, it’s a chemical reaction that produces temperatures higher than 170 degrees.</a></p>" // no comma here
+    },
+    { 
+        "q": "Defense attorney Norm London, who died last week at age 83, said his all-time favorite case involved the kidnapping of a chimpanzee who was named what?",
+        "a": [
+            {"option": "Mr. Moke",      "correct": true},
+            {"option": "Mr. Muggs",     "correct": false},
+            {"option": "Zippy",      "correct": false},
+            {"option": "Clyde",     "correct": false} // no comma here
+        ],
+        "correct": "<p><span>That's right!</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/norman-s-london-attorney-richthe-famous-and-downtrodden&quot;>Mr. Moke was a “talking” chimpanzee who was the star of the “monkey show” at the St. Louis Zoo. Four days before Christmas in 1959, the chimp’s former owner broke into the ape house and liberated him, but he later contacted London and arranged for the chimp to be returned, according to the obituary by Gloria Ross.</a></p>",
+        "incorrect": "<p><span>Incorrect.</span></p> <p><a href=&quot;http://news.stlpublicradio.org/post/norman-s-london-attorney-richthe-famous-and-downtrodden&quot;>Mr. Moke was a “talking” chimpanzee who was the star of the “monkey show” at the St. Louis Zoo. Four days before Christmas in 1959, the chimp’s former owner broke into the ape house and liberated him, but he later contacted London and arranged for the chimp to be returned, according to the obituary by Gloria Ross.</a></p>" // no comma here
+    }
+            ]
+};
